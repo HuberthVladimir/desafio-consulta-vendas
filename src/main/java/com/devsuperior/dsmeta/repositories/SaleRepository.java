ab8588895ca1a8d4de0790d0ea2b78004a04e885
Huberth Vladimir <vladimirhuberth@hotmail.com>
2025-01-29 20:51:48 -0300

    feat: added searchSummarySales custom query

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dsmeta.repositories;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.dto.SalesReportDTO;
 import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.projections.SalesSummaryProjection;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
@@ -22,4 +24,13 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
             """)
     Page<SalesReportDTO> searchSalesReport(Pageable page, LocalDate minDate, LocalDate maxDate,
             String name);
+
+    @Query(nativeQuery = true, value = """
+              SELECT SUM(TB_SALES.AMOUNT) AS total, TB_SELLER.NAME AS sellerName
+              FROM TB_SALES
+              INNER JOIN TB_SELLER ON TB_SALES.SELLER_ID = TB_SELLER.ID
+              WHERE TB_SALES.DATE BETWEEN :minDate AND :maxDate
+              GROUP BY TB_SELLER.NAME
+            """)
+    List<SalesSummaryProjection> searchSummarySales(LocalDate minDate, LocalDate maxDate);
 }
