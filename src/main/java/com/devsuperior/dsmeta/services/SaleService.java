a4189061995bb032516192caf126f67c1b2b8143
Huberth Vladimir <vladimirhuberth@hotmail.com>
2025-01-29 20:52:36 -0300

    feat: added searchSalesSummary and refactored SaleService

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -3,7 +3,9 @@ package com.devsuperior.dsmeta.services;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.dto.SalesReportDTO;
+import com.devsuperior.dsmeta.dto.SalesSummaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 
@@ -32,18 +35,33 @@ public class SaleService {
 				: LocalDate.parse(dateString);
 	}
 
-	public Page<SalesReportDTO> searchSalesReport(Pageable page, String minDate, String maxDate,
-			String name) {
+	private LocalDate[] getDefaultDateRange(String minDate, String maxDate) {
 		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
 		LocalDate oneYearAgo = today.minusYears(1L);
 
 		LocalDate startDate = this.parseDateOrDefault(minDate, oneYearAgo);
 		LocalDate endDate = this.parseDateOrDefault(maxDate, today);
 
+		return new LocalDate[] {startDate, endDate};
+	}
+
+	public Page<SalesReportDTO> searchSalesReport(Pageable page, String minDate, String maxDate,
+			String name) {
+		LocalDate[] dateRange = getDefaultDateRange(minDate, maxDate);
+		LocalDate startDate = dateRange[0];
+		LocalDate endDate = dateRange[1];
+
 		String searchName = (name == null || name.isEmpty()) ? "" : name;
 
-		Page<SalesReportDTO> result =
-				repository.searchSalesReport(page, startDate, endDate, searchName);
-		return result;
+		return repository.searchSalesReport(page, startDate, endDate, searchName);
+	}
+
+	public List<SalesSummaryDTO> searchSalesSummary(String minDate, String maxDate) {
+		LocalDate[] dateRange = this.getDefaultDateRange(minDate, maxDate);
+		LocalDate startDate = dateRange[0];
+		LocalDate endDate = dateRange[1];
+
+		return repository.searchSummarySales(startDate, endDate).stream().map(SalesSummaryDTO::new)
+				.collect(Collectors.toList());
 	}
 }
